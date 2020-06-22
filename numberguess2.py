d5eeeb1e929e477151634eabbb37669f630e9cce
Robert-Siberry <robert.siberry@outlook.com>
2020-06-22 14:18:40 +0100

    added a program file for the number guessing game

---
+++ numberguess2.py
@@ -0,0 +1,19 @@
+print("The Guessing Game")
+import random
+number=random.randint(1,9)
+guess=0
+counter=0
+while guess!=number and guess!="exit":
+    guess=input("Please guess a number between 1 and 9: ")
+    if guess == "exit":
+        break
+    guess = int(guess)
+    counter=counter+1
+    if guess < number:
+        print("Thats too low!")
+    elif guess > number:
+        print("Thats too high!")
+    else:
+        print("Thats right the number was",number,"and it only took you",counter,"guesses!")
+
+input()
