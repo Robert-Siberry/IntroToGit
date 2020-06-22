b9870e15bc26ddef5538e5d94cad3f99045c0044
Robert-Siberry <robert.siberry@outlook.com>
2020-06-22 15:55:05 +0100

    adding a program to dceide who the scrum master will be

---
+++ Scrmmaster.py
@@ -0,0 +1,16 @@
+import random
+
+input("Hit enter to decide who the next sctum master is")
+
+number = random,randint(1,4)
+
+if number = 1:
+	print("Srum master is Robert!")
+elseif number = 2:
+	print("Scrum master is David!")
+elseif number  = 3:
+	print("Scrum master is Paul!")
+else:
+	print("Scrum master is Tadas!")
+
+input()
